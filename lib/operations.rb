78ca405d89a05eef07e769ed9dfe031f087d65e8
Winston Tokuhisa <tokuhisawinston@outlook.com>
2018-07-01 17:03:53 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,10 @@
 require 'pry'
 
+# Use an if/else statement pair to build the unsafe?
+# method.
 def unsafe?(speed)
-  # Use an if/else statement pair to build the unsafe?
-  # method. It should return true if the speed is
-  # either below 40 or above 60.
+  # It should return true if the speed is either below
+  # 40 or above 60.
   if speed < 40 || speed > 60
     return true
   # Going 50 miles per hour, however, would return
@@ -13,9 +14,9 @@ def unsafe?(speed)
   end
 end
 
+# Build the method not_safe? that is a version of your
+# previous unsafe? method but use the ternary
+# operator (?:) instead of an if/else statement pair.
 def not_safe?(speed)
-  # Build the method not_safe? that is a version of
-  # your previous unsafe? method but use the ternary
-  # operator (?:) instead of an if/else statement pair.
   speed < 40 || speed > 60 ? true : false
 end
